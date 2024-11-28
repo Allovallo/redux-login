8e03a4ebc05bbdbb2b4a2af65f5207a6f6812778
Allovallo <grossco@meta.ua>
2024-11-28 17:37:10 +0200

    files updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
 
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Tasks from '../pages/Tasks';
+import { Layout } from './Layout';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const TasksPage = lazy(() => import('../pages/Tasks'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />}></Route>
-      <Route path="/tasks" element={<Tasks />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/tasks" element={<TasksPage />}></Route>
+      </Route>
     </Routes>
   );
 };
